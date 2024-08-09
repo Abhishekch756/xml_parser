e64f14c81a2d81cd40392572267a94de409e12e1
Abhishekch756 <abhishekcheekati@gmail.com>
2024-08-09 16:50:04 +0530

    deleted file

--- routes/homeRoutes.js
+++
@@ -1,8 +0,0 @@
-// homeRoutes.js
-const express = require("express");
-const mainController = require("../controllers/maincontroller");
-const router = express.Router();
-
-router.get("/", mainController.home);
-
-module.exports = router;
